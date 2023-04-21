3831fc59e2f9f20a103d3000b184f76ef6ed66b2
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-04-21 00:55:14 -0300

    feat: index files into pinecone and read with langchain

--- memory/convert_pdf_to_txt.py
+++ memory/convert_pdf_to_txt.py
@@ -1,7 +1,7 @@
 import PyPDF2
 
 # Open the PDF file in read-binary mode
-pdf_file_obj = open("test.pdf", "rb")
+pdf_file_obj = open("./memory/files/test.pdf", "rb")
 
 # Create a PdfReader object to read the PDF file
 pdf_reader = PyPDF2.PdfReader(pdf_file_obj)
@@ -10,15 +10,13 @@ pdf_reader = PyPDF2.PdfReader(pdf_file_obj)
 num_pages = len(pdf_reader.pages)
 
 # Open the output TXT file in write mode
-with open("output.txt", "w") as txt_file:
+with open("output.txt", "w", encoding="utf-8") as txt_file:
     # Iterate through the pages of the PDF file
     for page_num in range(num_pages):
         # Get the page object
         page_obj = pdf_reader.pages[page_num]
-
         # Extract the text from the page object
         text = page_obj.extract_text()
-
         # Write the extracted text to the output TXT file
         txt_file.write(text)
 
--- memory/retrieve_memory.py
+++ memory/retrieve_memory.py
@@ -1,3 +1,5 @@
+import gzip
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,13 +8,30 @@ import os
 from langchain import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
 from langchain.vectorstores import Pinecone
 from langchain.document_loaders import TextLoader
 
 import pinecone
 
 from testing_pinecone import init_pinecone
+
+
+def compress_metadata(metadata):
+    print("Compressing metadata")
+    metadata_str = json.dumps(metadata)
+    compressed_metadata = gzip.compress(metadata_str.encode("utf-8"))
+    return compressed_metadata
+
+
+def decompress_metadata(compressed_metadata):
+    print("Decompressing metadata")
+    decompressed_metadata_str = gzip.decompress(compressed_metadata).decode("utf-8")
+    return json.loads(decompressed_metadata_str)
+
 
 init_pinecone()
 
@@ -21,19 +40,26 @@ openai = OpenAI(
     openai_api_key=os.environ["OPENAI_API_KEY"],
 )
 
-loader = TextLoader("output.txt")
+loader = TextLoader("./memory/output.txt")
 documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+print(f"Loaded {len(documents)} documents")
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=20)
+print("Splitting documents")
 docs = text_splitter.split_documents(documents)
+print(f"Split {len(docs)} documents into {text_splitter} ")
 
 embeddings = OpenAIEmbeddings()
 
-
+print("Creating Pinecone index")
 index_name = "gptest"
-docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+docsearch = Pinecone.from_documents(
+    docs, embeddings, index_name=index_name, metadata_transform=compress_metadata
+)
 
-query = "Who is Cezar?"
-retrieved_docs = docsearch.similarity_search(query)
+query = "Quais alterações no cálculo de ICMS foram feitas?"
+retrieved_docs = docsearch.similarity_search(
+    query, metadata_transform=decompress_metadata
+)
 
 
 template = """Answer the question based on the context below. If the
